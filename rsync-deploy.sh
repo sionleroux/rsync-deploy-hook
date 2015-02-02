abc9845a587bff506ad5b5b5f4c9c258956a15a7
Siôn Le Roux <sinisterstuf@gmail.com>
2015-02-02 10:59:03 +0100

    Copy example update hook and modify it to suit my needs

--- rsync-deploy.sh
+++ rsync-deploy.sh
@@ -1,1 +1,42 @@
-#!/bin/bash
+#!/bin/sh
+#
+# An update hook that deploys the master branch via rsync.
+# Checks if the update is a commit on the master branch, then checks out the
+# files to a temporary directory and rsyncs them up to the web-server.
+#
+# TODO: compile SASS to CSS before rsyncing
+#
+
+# --- Command line
+refname="$1"
+oldrev="$2"
+newrev="$3"
+
+# --- Safety check
+if [ -z "$GIT_DIR" ]; then
+	echo "Don't run this script from the command line." >&2
+	echo " (if you want, you could supply GIT_DIR then run" >&2
+	echo "  $0 <ref> <oldrev> <newrev>)" >&2
+	exit 1
+fi
+
+if [ -z "$refname" -o -z "$oldrev" -o -z "$newrev" ]; then
+	echo "usage: $0 <ref> <oldrev> <newrev>" >&2
+	exit 1
+fi
+
+# --- Check types
+# if $newrev is 0000...0000, it's a commit to delete a ref.
+zero="0000000000000000000000000000000000000000"
+if [ "$newrev" = "$zero" ]; then
+	newrev_type=delete
+else
+	newrev_type=$(git cat-file -t $newrev)
+fi
+
+if [[ "$refname","$newrev_type" == "master,commit" ]]; then
+	#statements
+fi
+
+# --- Finished
+exit 0
